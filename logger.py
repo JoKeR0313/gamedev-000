94f504724c015d14b832e66bd3253856ad456fe7
anarch1986 <varga.tamas19860808@gmail.com>
2017-03-06 16:39:31 +0100

    ccLogger datetime string formatted for less characters

--- logger.py
+++ logger.py
@@ -5,7 +5,8 @@ import os
 class ccLogger:
     if not os.path.exists("logs"):
         os.makedirs("logs")
-    filename = open("logs/sci-fi_game_" + str(datetime.now()) + ".log", "w")
+    filename = open("logs/sci-fi_game_" +
+                    str(datetime.now())[:-7] + ".log", "w")
 
     def __init__(self, message):
         self.message = message
@@ -14,20 +15,20 @@ class ccLogger:
 
     @classmethod
     def error(cls, *args):
-        log = (str(datetime.now()), " !E! ", ' '.join(args), "\n")
+        log = (str(datetime.now())[:-3], " !E! ", ' '.join(args), "\n")
         ccLogger.filename.write(''.join(log))
-        print(log)
+        print(''.join(log))
 
     @classmethod
     def warning(cls, *args):
-        log = (str(datetime.now()), " !W! ", ' '.join(args), "\n")
+        log = (str(datetime.now())[:-3], " !W! ", ' '.join(args), "\n")
         ccLogger.filename.write(''.join(log))
-        print(log)
+        print(''.join(log))
 
     @classmethod
     def trace(cls, *args):
-        log = (str(datetime.now()), " !T! ", ' '.join(args), "\n")
-        print(log)
+        log = (str(datetime.now())[:-3], " !T! ", ' '.join(args), "\n")
+        print(''.join(log))
 
 ccLogger.error("Error", "Missing")
 ccLogger.warning("Warning", "Not have")
