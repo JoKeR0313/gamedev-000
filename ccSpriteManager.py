27bb394e218efd17f0bf400e37d32f6175c58eb1
Péter Soltész <peter.soltesz92@gmail.com>
2017-03-06 17:59:15 +0100

    fixed classmethod links

--- ccSpriteManager.py
+++ ccSpriteManager.py
@@ -12,15 +12,15 @@ class ccSpriteManager:
         # add the ccTexture to the textures dictionary, but only if it's not
         # already there (texture_name is the key). If it is there, don't add and
         # print a warning msg that says that we wanted to load it twice
-        if texture_name in ccSpriteManager.textures:
+        if texture_name in cls.textures:
             print("Warning: ccTexture is already loaded.")
         else:
-            ccSpriteManager.textures[texture_name] = texture
+            cls.textures[texture_name] = texture
 
     @classmethod
     def get_texture(cls, texture_name):
         # give back the ccTexture. If it can't be found, write an error msg and return with None
-        texture = ccSpriteManager.textures.get(texture_name)
+        texture = cls.textures.get(texture_name)
         if texture:
             return texture
         else:
@@ -31,15 +31,15 @@ class ccSpriteManager:
         # add ccSprite to sprites dictionary but only if it is not there
         # (sprite_name is the key). If it is there, print a warning msg and don't
         # overwrite the previous one
-        if sprite_name in ccSpriteManager.sprites:
+        if sprite_name in cls.sprites:
             print("Warning: ccSprite is already loaded. It will not be overwritten.")
         else:
-            ccSpriteManager.sprites[sprite_name] = sprite
+            cls.sprites[sprite_name] = sprite
 
     @classmethod
     def get_sprite(cls, sprite_name):
         # give back the ccSprite. If it can't be found, write an error msg and return with None
-        sprite = ccSpriteManager.sprites.get(sprite_name)
+        sprite = cls.sprites.get(sprite_name)
         if sprite:
             return sprite
         else:
