2702a68b3de537ee4a17d511e71d3acc347b0020
anarch1986 <varga.tamas19860808@gmail.com>
2017-03-07 17:56:55 +0100

    ccLogger filewriting method modified

--- game/cc_logger.py
+++ game/cc_logger.py
@@ -6,8 +6,8 @@ class ccLogger:
     if not os.path.exists("logs"):
         os.makedirs("logs")
     bad_chars = (":", " ")
-    filename = open("logs/sci-fi_game_" +
-                    str(datetime.now()).replace(":", "_").replace(" ", "_")[:-7] + ".log", "w")
+    filename = "logs/sci-fi_game_" + \
+        str(datetime.now()).replace(":", "_").replace(" ", "_")[:-7] + ".log"
 
     def __init__(self, message):
         self.message = message
@@ -17,16 +17,19 @@ class ccLogger:
     @classmethod
     def error(cls, *args):
         log = (str(datetime.now())[:-3], " !E! ", ' '.join(args), "\n")
-        ccLogger.filename.write(''.join(log))
+        with open(ccLogger.filename, 'a') as writefile:
+            writefile.write(''.join(log))
         print(''.join(log))
 
     @classmethod
     def warning(cls, *args):
         log = (str(datetime.now())[:-3], " !W! ", ' '.join(args), "\n")
-        ccLogger.filename.write(''.join(log))
+        with open(ccLogger.filename, 'a') as writefile:
+            writefile.write(''.join(log))
         print(''.join(log))
 
     @classmethod
     def trace(cls, *args):
         log = (str(datetime.now())[:-3], " !T! ", ' '.join(args), "\n")
-        ccLogger.filename.write(''.join(log))
+        with open(ccLogger.filename, 'a') as writefile:
+            writefile.write(''.join(log))
