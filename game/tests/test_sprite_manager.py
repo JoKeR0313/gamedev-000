3a3fcc9c3dec4cfeacb32b9dccf4542fce1cc123
Laszlo Molnar <laszlo.molnar@codecool.com>
2017-03-13 14:33:35 +0100

    - the sprite manager test is a little bit more interesting now

--- game/tests/test_sprite_manager.py
+++ game/tests/test_sprite_manager.py
@@ -5,11 +5,12 @@ from cc_texture import ccTexture
 from cc_sprites_file_loader import ccSpritesFileLoader
 from cc_sprite_manager import ccSpriteManager
 
-
 class TestSpriteManager:
     size = (840, 480)
     sprites = []
     renderer = None
+    num_of_rows = 1
+    num_changer = 1
 
     def __init__(self):
         pygame.init()
@@ -20,6 +21,7 @@ class TestSpriteManager:
         self.__get_sprites_from_sprite_manager()
 
     def test_run(self):
+        clock = pygame.time.Clock()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -28,14 +30,28 @@ class TestSpriteManager:
             self.renderer.fill((0, 0, 0))
             self.__draw_sprites()
             pygame.display.flip()
+            clock.tick(30)
 
     def __draw_sprites(self):
-        for i in range(len(self.sprites)):
-            width = self.sprites[i].rectangle[0]
-            height = self.sprites[i].rectangle[1]
-            self.sprites[i].draw(self.renderer, i*width, i*height)
+        for j in range(self.num_of_rows):
+            for i in range(len(self.sprites)):
+                width = self.sprites[i].rectangle[0]
+                height = self.sprites[i].rectangle[1]
+                self.sprites[i].draw(self.renderer, i*width+j*width, i*height)
+                self.sprites[i].draw(self.renderer,
+                                     (len(self.sprites)-i-1)*width+j*width,
+                                     len(self.sprites)*height+i*height)
+        self.__change_num_of_rows()
+
+    def __change_num_of_rows(self):
+        self.num_of_rows += self.num_changer
+        if self.num_of_rows > 20:
+            self.num_changer = -1
+        if self.num_of_rows <= 1:
+            self.num_changer = 1
 
     def __load_sprites(self):
+        # we are in the /test directory, so we need to go back
         ccTexture.resource_dir_path = "../resources/"
         loader = ccSpritesFileLoader()
         loader.process_file("../resources/sprites/test.sprites.json")
@@ -45,7 +61,6 @@ class TestSpriteManager:
         self.sprites.append(ccSpriteManager.get_sprite("test_02"))
         self.sprites.append(ccSpriteManager.get_sprite("test_03"))
         self.sprites.append(ccSpriteManager.get_sprite("test_all_001"))
-
 
 if '__main__' == __name__:
     test = TestSpriteManager()
