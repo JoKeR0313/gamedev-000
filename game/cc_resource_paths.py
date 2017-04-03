71849a134f79ed84a0061801adebb31b76322ce5
davidkulchar <davidkulchar@gmail.com>
2017-04-03 19:39:13 +0200

    simlyfied the res_path, and hopefully resolved the mentioned probs in Act

--- game/cc_resource_paths.py
+++ game/cc_resource_paths.py
@@ -4,6 +4,7 @@ import cc_logger
 class ccResourcePaths:
 
     base_path = os.path.dirname(os.path.realpath(__file__))
+    resource_path = base_path + "/resources/"
 
     def __init__(self):
         ccLogger.error("ccResourcePath can not be instanted")
@@ -11,26 +12,26 @@ class ccResourcePaths:
 
     @classmethod
     def get_resources(cls):
-        return cls.base_path + "/resources/"
+        return cls.resource_path
 
     @classmethod
     def get_objects(cls):
-        return cls.base_path + "/resources/objects/"
+        return cls.resource_path  + "objects/"
 
     @classmethod
     def get_sprites(cls):
-        return cls.base_path + "/resources/sprites/"
+        return cls.resource_path + "sprites/"
 
     @classmethod
     def get_anims(cls):
-        return cls.base_path + "/resources/anims/"
+        return cls.resource_path + "anims/"
 
     @classmethod
     def get_object_scenes(cls):
-        return cls.base_path + "/resources/object_scenes/"
+        return cls.resource_path + "object_scenes/"
 
     @classmethod
     def get_acts(cls):
-        return cls.base_path + "/resources/acts/"
+        return cls.resource_path + "acts/"
 
 
