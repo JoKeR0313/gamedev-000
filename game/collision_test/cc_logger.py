0676b18579f3e7205426c875317eaf32a723edd4
anarch1986 <varga.tamas19860808@gmail.com>
2017-04-04 17:02:51 +0200

    file system created for testing

---
+++ game/collision_test/cc_logger.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import os
+
+
+class ccLogger:
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+    bad_chars = (":", " ")
+    filename = "logs/sci-fi_game_" + \
+        str(datetime.now()).replace(":", "_").replace(" ", "_")[:-7] + ".log"
+
+    def __init__(self, message):
+        self.message = message
+        print(message)
+        raise Exception(message)
+
+    @classmethod
+    def error(cls, *args):
+        log = (str(datetime.now())[:-3], " !E! ",
+               ' '.join(str(arg)for arg in args), "\n")
+        with open(ccLogger.filename, 'a') as writefile:
+            writefile.write(''.join(log))
+        print(''.join(log))
+
+    @classmethod
+    def warning(cls, *args):
+        log = (str(datetime.now())[:-3], " !W! ",
+               ' '.join(str(arg)for arg in args), "\n")
+        with open(ccLogger.filename, 'a') as writefile:
+            writefile.write(''.join(log))
+        print(''.join(log))
+
+    @classmethod
+    def trace(cls, *args):
+        log = (str(datetime.now())[:-3], " !T! ",
+               ' '.join(str(arg)for arg in args), "\n")
+        with open(ccLogger.filename, 'a') as writefile:
+            writefile.write(''.join(log))
