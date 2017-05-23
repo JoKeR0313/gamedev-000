c85eafb4e4e948721475b16e0a8f11cddbf7912c
Péter Vetési <vetesipeti@gmail.com>
2017-05-23 18:05:20 +0200

    test_file_Scene_file_loader.py: slightly modified

--- game/tests/test_tile_scene_file_loader.py
+++ game/tests/test_tile_scene_file_loader.py
@@ -1,10 +1,14 @@
 import import_dir_setter
 from cc_tile_scene_file_loader import ccTileSceneFileLoader
 
+from game.cc_tile_map_scene import ccTileMapScene
+
+
 def TestTileSceneLoader():
     loader = ccTileSceneFileLoader()
     loader.process_file("test.tilescene.json")
-    print(loader.objects_dict)
-    print(loader.map)
+    print("gdasvd", loader.objects_dict)
+    print("map: ", loader.map)
+
 
 TestTileSceneLoader()
