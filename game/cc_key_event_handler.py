758f15390ba2faab33f177d3b050c32b975b7105
Mátyás Krista <nincs.matyas@gmail.com>
2017-05-30 12:29:24 +0200

    Working on key event handler.

--- game/cc_key_event_handler.py
+++ game/cc_key_event_handler.py
@@ -3,33 +3,34 @@ import pygame
 
 class ccKeyEventHandler:
 
-    def step(self, time_passed):
+    def __init__(self):
+        self.right = pygame.K_RIGHT
+        self.left = pygame.K_LEFT
+        self.up = pygame.K_UP
+        self.down = pygame.K_DOWN
+        self.fire = pygame.K_SPACE
+        self.is_right_pressed = False
+        self.is_left_pressed = False
+        self.is_up_pressed = False
+        self.is_down_pressed = False
+        self.is_fire_pressed = False
+
+    def update(self):
         pressed = pygame.key.get_pressed()
-        for obj in self.object_list:
-            obj.step(time_passed)
-            if obj.id == 200:
-                obj_tomove = obj
-                if pressed[pygame.K_LEFT]:
-                    print("LEFT")
-                    # obj_tomove.position[0] -= 1
-                    # self.update_obj_tomove()
-                    # if self.collision_check():
-                    #     obj_tomove.position[0] += 1
-                if pressed[pygame.K_RIGHT]:
-                    print("RIGHT")
-                    # obj_tomove.position[0] += 1
-                    # self.update_obj_tomove()
-                    # if self.collision_check():
-                    #     obj_tomove.position[0] -= 1
-                if pressed[pygame.K_UP]:
-                    print("UP")
-                    # obj_tomove.position[1] -= 1
-                    # self.update_obj_tomove()
-                    # if self.collision_check():
-                    #     obj_tomove.position[1] += 1
-                if pressed[pygame.K_DOWN]:
-                    print("DOWN")
-                    # obj.position[1] += 1
-                    # self.update_obj_tomove()
-                    # if self.collision_check():
-                    #     obj_tomove.position[1] -=
+        self.reset_keys()
+        if pressed[self.left]:
+            self.is_left_pressed = True
+        if pressed[self.right]:
+            self.is_right_pressed = True
+        if pressed[self.up]:
+            self.is_up_pressed = True
+        if pressed[self.down]:
+            self.is_down_pressed = True
+        if pressed[self.fire]:
+            self.is_fire_pressed = True
+        print(self.is_down_pressed, self.is_up_pressed, self.is_right_pressed, self.is_left_pressed)
+
+    def reset_keys(self):
+        for attribute in dir(self):
+            if attribute.startswith('is'):
+                self.__setattr__(attribute, False)
---
+++ game/tests/test_key_event_handler.py
@@ -0,0 +1,40 @@
+import pygame
+
+from cc_act_manager import ccActManager
+from cc_globals import ccGlobals
+from cc_key_event_handler import ccKeyEventHandler
+
+
+class TestActLoader:
+    size = (440, 480)
+    num_of_rows = 1
+    num_changer = 1
+
+    def __init__(self):
+        pygame.init()
+        renderer = pygame.display.set_mode(self.size)
+        ccGlobals.set_renderer(renderer)
+        ccGlobals.size = self.size
+
+    def test_run(self):
+        keylistener = ccKeyEventHandler()
+        ccActManager.load("test.act.json")
+        clock = pygame.time.Clock()
+        time_passed = 0
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            keylistener.update()
+            ccActManager.step(time_passed)
+            ccGlobals.get_renderer().fill((0, 0, 0))
+            ccActManager.draw()
+            pygame.display.flip()
+            time_passed = clock.tick(30)
+
+
+def main():
+    tester = TestActLoader()
+    tester.test_run()
+
+main()
