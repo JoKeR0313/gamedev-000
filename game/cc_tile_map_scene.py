00aa25ca709d1da7e0699ea5d4f231554b53ab09
Varga Tamás <varga.tamas19860808@gmail.com>
2017-06-13 16:47:50 +0200

    scrolling features moved to cc_tile_map_scene.py file

--- game/cc_tile_map_scene.py
+++ game/cc_tile_map_scene.py
@@ -10,16 +10,17 @@ class ccTileMapScene(ccScene):
 
     def __init__(self):
         super().__init__()
-        # self.object_list = []
         self.map = []
         self.type = "ccTileMapScene"
         self.offset = []
         self.velocity = pygame.math.Vector2(0, 0)
+        self.tile_width = 1
 
     def load(self, filename):
         loader = ccTileSceneFileLoader()
         loader.process_file(filename)
         self.map = loader.get_map()
+        self.tile_width = self.map[0][0].active_sprite.rectangle.width
         self.offset = loader.get_offset()
 
     def draw(self):
@@ -28,6 +29,10 @@ class ccTileMapScene(ccScene):
                 obj.draw(ccGlobals.get_renderer())
 
     def step(self, time_passed):
-        # for obj in self.object_list:
-        #     obj.step(time_passed)
-        pass
+        self.first_tile_pos += self.velocity.x * time_passed
+        local_p = int(self.first_tile_pos)
+
+        for row in self.map:
+            for x in range(len(row)):
+                obj = row[x]
+                obj.position.x = local_p + x * self.tile_width
