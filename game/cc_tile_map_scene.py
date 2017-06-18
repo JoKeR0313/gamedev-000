6071507330351d4897cdf1feb9f0c58c9bfe737f
Varga Tamás <varga.tamas19860808@gmail.com>
2017-06-18 11:40:48 +0200

    looping switch added to tile_map_scene.py

--- game/cc_tile_map_scene.py
+++ game/cc_tile_map_scene.py
@@ -16,6 +16,7 @@ class ccTileMapScene(ccScene):
         self.velocity = pygame.math.Vector2(0, 0)
         self.tile_width = 1
         self.first_tile_pos = 0  # use offset here
+        self.looping = False
 
     def load(self, filename):
         loader = ccTileSceneFileLoader()
@@ -23,6 +24,7 @@ class ccTileMapScene(ccScene):
         self.map = loader.get_map()
         self.tile_width = self.map[0][0].active_sprite.rectangle.width
         self.offset = loader.get_offset()
+        self.looping = loader.get_looping()
 
     def draw(self):
         contained_length = 0
@@ -32,21 +34,24 @@ class ccTileMapScene(ccScene):
             contained_length = end_row_index - len(self.map[0])
             end_row_index = len(self.map[0])
 
+        
         for row in self.map:
             for index in range(start_row_index, end_row_index):
                 row[index].draw(ccGlobals.get_renderer())
 
-        if contained_length != 0:
-            for row in self.map:
-                for index in range(0,contained_length):
-                    row[index].draw(ccGlobals.get_renderer())
+        if self.looping is True:
+            if contained_length != 0:
+                for row in self.map:
+                    for index in range(0,contained_length):
+                        row[index].draw(ccGlobals.get_renderer())
 
 
     def step(self, time_passed):
         contained_length = 0
         self.first_tile_pos += self.velocity.x * time_passed
-        if abs(self.first_tile_pos) >= len(self.map[0]) * self.tile_width:
-            self.first_tile_pos += len(self.map[0]) * self.tile_width
+        if self.looping is True:
+            if abs(self.first_tile_pos) >= len(self.map[0]) * self.tile_width:
+                self.first_tile_pos += len(self.map[0]) * self.tile_width
 
         local_p = int(self.first_tile_pos)
 
@@ -56,18 +61,15 @@ class ccTileMapScene(ccScene):
             contained_length = end_row_index - len(self.map[0])
             end_row_index = len(self.map[0])
 
+        
         for row in self.map:
             for index in range(start_row_index, end_row_index):
                 row[index].position.x = local_p + index * self.tile_width
 
 
-        if contained_length != 0:
-            local_p = local_p + (len(self.map[0])*self.tile_width)
-            for row in self.map:
-                for index in range(0,contained_length):
-                    row[index].position.x = local_p + index * self.tile_width
-
-
-
-
-
+        if self.looping is True:
+            if contained_length != 0:
+                local_p = local_p + (len(self.map[0])*self.tile_width)
+                for row in self.map:
+                    for index in range(0,contained_length):
+                        row[index].position.x = local_p + index * self.tile_width
