90497ec7a43fe9c0b3c6bf7df7eebdd1f98b447b
Varga Tamás <varga.tamas19860808@gmail.com>
2017-07-11 15:42:14 +0200

    collision detector refactored

--- game/cc_collision_detector.py
+++ game/cc_collision_detector.py
@@ -2,15 +2,6 @@ import pygame
 from cc_object_scene import ccObjectScene
 from cc_tile_map_scene import ccTileMapScene
 
-"""
-update(list of scenes (cls.scene)):
-first scene first obj if hitbox is not None: 
-if collide anything:
-object.objecthit()
-
-for range(scene)
-"""
-
 
 class ccCollisionDetector:
 
@@ -18,42 +9,45 @@ class ccCollisionDetector:
     def update(cls, scenes):
         for i in range(len(scenes)):
             if issubclass(type(scenes[i]), ccObjectScene):
-                for current_obj in scenes[i].object_list:
-                    if current_obj.hitbox is not None:
-                        if i <= len(scenes):
-                            for j in range(i + 1, len(scenes)):
-                                if issubclass(type(scenes[j]), ccObjectScene):
-                                    for obj in scenes[j].object_list:
-                                        if obj.hitbox is not None:
-                                            if current_obj.hitbox.colliderect(obj.hitbox):
-                                                current_obj.objecthit(obj)
-                                                obj.objecthit(current_obj)
+                ccCollisionDetector.update_object_scene(scenes, i)
+            elif issubclass(type(scenes[i]), ccTileMapScene):
+                ccCollisionDetector.update_tilemap_scene(scenes, i)
 
-                                if issubclass(type(scenes[j]), ccTileMapScene):
-                                    for m in range(len(scenes[i].map)):
-                                        for current_obj in scenes[j].map[m]:
-                                            if obj.hitbox is not None:
-                                                if current_obj.hitbox.colliderect(obj.hitbox):
-                                                    current_obj.objecthit(obj)
-                                                    obj.objecthit(current_obj)
+    @classmethod
+    def update_object_scene(cls, scenes, i):
+        for current_obj in scenes[i].object_list:
+            if current_obj.hitbox is not None:
+                if i <= len(scenes):
+                    for j in range(i + 1, len(scenes)):
+                        if issubclass(type(scenes[j]), ccObjectScene):
+                            ccCollisionDetector.object_scene_collision(scenes, j, current_obj)
+                        elif issubclass(type(scenes[j]), ccTileMapScene):
+                            ccCollisionDetector.tilemap_scene_collision(scenes,j,current_obj)
 
-            if issubclass(type(scenes[i]), ccTileMapScene):
-                for m in range(len(scenes[i].map)):
-                    for current_obj in scenes[i].map[m]:
-                        if current_obj.hitbox is not None:
-                            for j in range(i + 1, len(scenes)):
+    @classmethod
+    def update_tilemap_scene(cls, scenes, i):
+        for m in range(len(scenes[i].map)):
+            for current_obj in scenes[i].map[m]:
+                if current_obj.hitbox is not None:
+                    for j in range(i + 1, len(scenes)):
+                        if issubclass(type(scenes[j]), ccObjectScene):
+                            ccCollisionDetector.object_scene_collision(scenes, j, current_obj)
+                        elif issubclass(type(scenes[j]), ccTileMapScene):
+                            ccCollisionDetector.tilemap_scene_collision(scenes,j,current_obj)
 
-                                if issubclass(type(scenes[j]), ccObjectScene):
-                                    for obj in scenes[j].object_list:
-                                        if obj.hitbox is not None:
-                                            if current_obj.hitbox.colliderect(obj.hitbox):
-                                                current_obj.objecthit(obj)
-                                                obj.objecthit(current_obj)
+    @classmethod
+    def object_scene_collision(cls, scenes, j, current_obj):
+        for obj in scenes[j].object_list:
+            if obj.hitbox is not None:
+                if current_obj.hitbox.colliderect(obj.hitbox):
+                    current_obj.objecthit(obj)
+                    obj.objecthit(current_obj)
 
-                                if issubclass(type(scenes[j]), ccTileMapScene):
-                                    for m in range(len(scenes[i].map)):
-                                        for current_obj in scenes[j].map[m]:
-                                            if obj.hitbox is not None:
-                                                if current_obj.hitbox.colliderect(obj.hitbox):
-                                                    current_obj.objecthit(obj)
-                                                    obj.objecthit(current_obj)
+    @classmethod
+    def tilemap_scene_collision(cls, scenes, j, current_obj):
+        for m in range(len(scenes[i].map)):
+            for current_obj in scenes[j].map[m]:
+                if obj.hitbox is not None:
+                    if current_obj.hitbox.colliderect(obj.hitbox):
+                        current_obj.objecthit(obj)
+                        obj.objecthit(current_obj)
