5ea6b8008c604bdf02c92dd0ff9f367d29ca2d9e
JoKeR0313 <istvanfilimon@gmail.com>
2017-07-11 17:58:37 +0200

    King is jumping and falling.

--- game/tests/test_king_object.py
+++ game/tests/test_king_object.py
@@ -8,12 +8,14 @@ class KingObject(ccAnimObject):
     def __init__(self):
         super().__init__()
         self.type = 'KingObject'
+        self.jumping = False
 
     def step(self, time_passed):
         if ccKeyEventHandler.get_is_right_pressed():
             self.play()
         else:
             self.pause()
+        self.jump()
         super().step(time_passed)
 
     def copy(self):
@@ -26,3 +28,16 @@ class KingObject(ccAnimObject):
         self.velocity.y = 0
         self.hitbox.y = other_obj.hitbox.y - self.hitbox.height
         self.position.y -= local_hitbox_y - self.hitbox.y
+
+    def jump(self):
+        if self.jumping is False and ccKeyEventHandler.get_is_up_pressed():
+            self.velocity.y = -2
+            self.jumping = True
+        if self.jumping is True:
+            self.velocity.y *= 0.5
+
+        if self.jumping is True and self.velocity.y > -0.05:
+            self.jumping = False
+            self.velocity.y = 0.25
+        if self.jumping is False:
+            self.velocity.y *= 1.5
